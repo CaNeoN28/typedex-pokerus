2794d5098eceb11841aa609df7462fbfda242452
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2022-05-10 14:05:26 -0300

    Atualizando estilos do FormsCard

--- src/pages/Pokemon/Main.tsx
+++ src/pages/Pokemon/Main.tsx
@@ -44,7 +44,7 @@ export default function MainInfo({ species, forms, form, setForm }: Props) {
   return (
     <InfoPage>
       <FormsCard>
-        <img src={img || ''} alt={currentForm.name} />
+        <img className="image" src={img || ''} alt={currentForm.name} />
         {forms.length > 1 && <div className='optionBox'>
           <a onClick={e => form === 0 ? setForm(forms.length - 1) : setForm(form - 1)}><AiFillCaretLeft /></a>
           <span> {currentForm.name} </span>

7dae7542407c4353e063dd18664084fb28dac37e
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2022-05-13 17:26:16 -0300

    Universalização do componente OptionBox

--- src/pages/Pokemon/Main.tsx
+++ src/pages/Pokemon/Main.tsx
@@ -66,12 +66,14 @@ export default function MainInfo({ species, forms, form, setForm }: Props) {
   return (
     <InfoPage>
       <FormsCard>
-        <img src={img} alt={currentForm.name} />
+        <img src={img} alt={currentForm.name}/>
+        {forms.length > 1 && <OptionBox label={[f.capitalize(currentForm.name)]} comparing={forms.length}/>}
+        {/* <img src={img} alt={currentForm.name} />
         {forms.length > 1 && <div className='optionBox'>
           <a onClick={e => form === 0 ? setForm(forms.length - 1) : setForm(form - 1)}><AiFillCaretLeft /></a>
           <span> {f.formattingFormName(species.name, currentForm.name)} </span>
           <a onClick={e => form === forms.length - 1 ? setForm(0) : setForm(form + 1)}><AiFillCaretRight /></a>
-        </div>}
+        </div>} */}
       </FormsCard>
       <div>
         <OptionBox label={[`N° ${String(species.id).padStart(3, '0')}`, f.capitalize(species.name)]} comparing={897}/>

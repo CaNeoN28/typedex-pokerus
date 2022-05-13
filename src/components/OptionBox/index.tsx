77ebf9558c53afdcb7113a965a50f592ac5b253a
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2022-05-13 18:16:59 -0300

    Refatorando e aplicando o OptionBox

--- src/components/OptionBox/index.tsx
+++ src/components/OptionBox/index.tsx
@@ -2,20 +2,36 @@ import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai"
 import './OptionBox.css'
 
 interface Props{
+  type: "species" | "form"
   label: string[]
   comparing: number
   parameter: number
   setParameter: React.Dispatch<React.SetStateAction<number>>
 }
 
-export default function OptionBox({label, comparing, parameter, setParameter} : Props){
+export default function OptionBox({type, label, comparing, parameter, setParameter} : Props){
+
+  const onClickDefault = (min : number, operation : "minus" | "plus") => {
+    if (operation == "minus")
+      parameter == min ? setParameter(comparing) : setParameter(parameter - 1)
+    else if (operation == "plus")
+      parameter == comparing ? setParameter(min) : setParameter(parameter + 1)
+  }
+
+  const onClick = (operation: "minus" | "plus") => {
+    if (type === "species")
+      onClickDefault(1, operation)
+    else if (type === "form")
+      onClickDefault(0, operation)
+  }
+
   return(
     <div className="optionBox">
-      <a onClick={e => parameter == 1 ? setParameter(comparing) : setParameter(parameter - 1)}><AiFillCaretLeft /></a>
+      {<a onClick={e=> onClick("minus")}><AiFillCaretLeft /></a>}
       <div className="optionLabelGroup">
         {label.map(l => <span>{l}</span>)}
       </div>
-      <a onClick={e => parameter == comparing ? setParameter(1) : setParameter(parameter + 1)}><AiFillCaretRight /></a>
+      <a onClick={e => onClick("plus")}><AiFillCaretRight /></a>
     </div>
   )
 }
--- src/pages/Pokemon/Main.tsx
+++ src/pages/Pokemon/Main.tsx
@@ -21,7 +21,7 @@ interface Props {
 export default function MainInfo({ species, forms, form, setForm }: Props) {
   const f = Formatting
 
-  const currentForm = forms[form - 1];
+  const currentForm = forms[form];
   const img = currentForm.sprites.other["official-artwork"].front_default || missingNo
 
   const actualHeight = currentForm.height / 10
@@ -66,14 +66,14 @@ export default function MainInfo({ species, forms, form, setForm }: Props) {
   return (
     <InfoPage>
       <FormsCard>
-        <img src={img} alt={currentForm.name}/>
-        {forms.length > 1 && <OptionBox label={[f.capitalize(currentForm.name)]} comparing={forms.length} parameter={form} setParameter={setForm}/>}
-        {/* <img src={img} alt={currentForm.name} />
-        {forms.length > 1 && <div className='optionBox'>
-          <a onClick={e => form === 0 ? setForm(forms.length - 1) : setForm(form - 1)}><AiFillCaretLeft /></a>
-          <span> {f.formattingFormName(species.name, currentForm.name)} </span>
-          <a onClick={e => form === forms.length - 1 ? setForm(0) : setForm(form + 1)}><AiFillCaretRight /></a>
-        </div>} */}
+        <img src={img} alt={currentForm.name} />
+        {forms.length > 1 && <OptionBox
+          type={'form'}
+          label={[f.formattingFormName(species.name, currentForm.name)]}
+          comparing={forms.length - 1}
+          parameter={form}
+          setParameter={setForm}
+        />}
       </FormsCard>
       <div>
         {/*</InfoPage>OptionBox label={[`N° ${String(species.id).padStart(3, '0')}`, f.capitalize(species.name)]} comparing={897}/>*/}

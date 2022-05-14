6c554f7cc8dc24a6bbd9e3d994803aeffed7b538
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2022-05-14 20:31:50 -0300

    Mudando a maneira de pegar as formas

--- src/pages/Pokemon/index.tsx
+++ src/pages/Pokemon/index.tsx
@@ -6,28 +6,25 @@ import MainInfo from "./Main";
 
 export default function PokemonPage() {
   const { id } = useParams();
-  const navigate = useNavigate();
   const api = new PokemonClient();
 
   const [species, setSpecies] = useState<PokemonSpecies>();
-  const [form, setForm] = useState(0)
   const [forms, setForms] = useState<Pokemon[]>()
   const [current_form, setCurrentForm] = useState<Pokemon>();
   const [max_pokemon, setMaxPokemon] = useState(0)
   const [id_copy, setIdCopy] = useState<number>(0)
 
-  const getSpecies = async() => {
+  const getSpecies = async () => {
     await api.getPokemonSpeciesByName(id || '')
       .then(res => setSpecies(res))
   }
 
-  const getForms = async() => {
+  const getForms = async () => {
     await species?.varieties.map(v => {
       api.getPokemonByName(v.pokemon.name)
         .then(res => setForms(
-          oldForms => (oldForms && !oldForms.find(p => res.id == p.id) ? [...oldForms, res] : [res])
-            .filter(form => form.species.name === species.name)
-            .sort((a, b) => (a.id < b.id) ? -1 : 1))
+          oldForms => (oldForms && !oldForms.find(p => res.id == p.id) ? [...oldForms
+            .filter(form => form.species.name === species?.name), res] : [res]))
         )
     })
 
@@ -46,23 +43,18 @@ export default function PokemonPage() {
   useEffect(() => {
     getForms()
   }, [species])
-  
-  useEffect(() => {
-    setForm(0)
-    if (id_copy != species?.id && id_copy != 0) {
-      navigate(`/pokemon/${id_copy}`)
-    }
-  }, [id_copy])
 
   useEffect(() => {
-    if (forms)
-      setCurrentForm(forms[form])
-  }, )
+    if (forms){
+      forms.sort((a, b) => a.id < b.id ? -1 : 1)
+      setCurrentForm(forms[0])
+    }
+  }, [forms])
 
   if (species && forms && current_form)
     return (
       <Page>
-        <MainInfo max_pokemon={max_pokemon} species={species} forms={forms} current_form={current_form}/>
+        <MainInfo max_pokemon={max_pokemon} species={species} forms={forms} current_form={current_form} />
       </Page>
     )
 

14190ad5107e13389059b1b943821a601b706fcf
CaNeoN28 <carlosfelipe.st28@gmail.com>
2022-05-16 21:24:36 -0400

    Pegando dicionário de pokémons

--- src/pages/ListPokemon/index.tsx
+++ src/pages/ListPokemon/index.tsx
@@ -1,15 +1,17 @@
 import Page from "components/Page";
 import SearchBox from "components/Searchbox";
 import { useEffect, useState } from "react";
-import { Pokemon, PokemonClient, PokemonSpecies } from 'pokenode-ts'
+import { NamedAPIResource, Pokemon, PokemonClient, PokemonSpecies } from 'pokenode-ts'
 import LoadButton from "components/LoadButton";
 
 export default function () {
   const api = new PokemonClient();
 
-  const [max, setMax] = useState(16)
+  const min = 16
+  const [max, setMax] = useState(min)
 
   const [search, setSearch] = useState('')
+  const [pokemon_dict, setPokemonDict] = useState<NamedAPIResource[]>()
   const [pokemon_list, setPokemonList] = useState<PokemonSpecies[]>()
 
   const validateIfHasPokemon = (oldList: PokemonSpecies[], species: PokemonSpecies) => {
@@ -19,39 +21,22 @@ export default function () {
     return [...oldList]
   }
 
-  const getSpeciesList = async () => {
-    await api.listPokemonSpecies(0, 10000)
-      .then(res => res.results.map(r => {
-        api.getPokemonSpeciesByName(r.name)
-          .then(species => {
-            setPokemonList(oldList => oldList ? validateIfHasPokemon(oldList, species) : [species])
-          })
-      }))
+  const getPokemonDict = async () => {
+    await api.listPokemons(0, 10000)
+      .then(pokemon => setPokemonDict(pokemon.results))
   }
 
   useEffect(() => {
-  }, [search])
-
-  useEffect(() => {
-    if (pokemon_list)
-      pokemon_list.sort((a, b) => a.id < b.id ? -1 : 1)
-  }, [pokemon_list])
-  useEffect(() => {
-    getSpeciesList()
+    getPokemonDict()
   }, [])
 
-  if (pokemon_list)
-    return (
-      <Page>
-        <SearchBox setSearch={setSearch} />
-        <ul>
-          {pokemon_list.map((species, index) => (index < max) && <li key={index}>{species.name}</li>)}
-        </ul>
-        <LoadButton max={max} setMax={setMax} true_max={pokemon_list.length}/>
-      </Page>
-    )
-
   return (
-    <></>
+    <Page>
+      <SearchBox setSearch={setSearch} />
+      <ul>
+        {pokemon_dict && pokemon_dict.map((species, index) => (index < max) && <li key={index}>{species.name}</li>)}
+      </ul>
+      {pokemon_dict && <LoadButton max={max} setMax={setMax} true_max={pokemon_dict.length}/>}
+    </Page>
   )
 }

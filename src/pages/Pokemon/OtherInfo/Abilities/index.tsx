ea39d8d147acdb6205d1b5c046225f9b3fefbe33
CaNeoN28 <carlosfelipe.st28@gmail.com>
2022-05-26 21:30:37 -0400

    Formatando as habilidades

--- src/pages/Pokemon/OtherInfo/Abilities/index.tsx
+++ src/pages/Pokemon/OtherInfo/Abilities/index.tsx
@@ -1,17 +1,19 @@
 import Formatting from "common/utils/string";
 import { PokemonAbility } from "pokenode-ts";
+import {AiFillStar} from 'react-icons/ai';
+import './Abilities.scss';
 
 export default function(props : {abilities : PokemonAbility[]}){
   const abilities = props.abilities
   const f = Formatting
 
   return(
-    <>
+    <div className="abilities">
       {abilities.map((a, index) => 
-        <div key={index}>
-          {f.capitalize(f.compostString(a.ability.name))}
-        </div>
+        <a key={index} className='ability'>
+          {f.capitalize(f.compostString(a.ability.name))} {a.is_hidden && <AiFillStar/>}
+        </a>
       )}
-    </>
+    </div>
   )
 }

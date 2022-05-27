9b3566124a06b718fa818c1a32df12610c717f8e
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2022-05-27 12:53:25 -0300

    Usando o chartjs

---
+++ src/pages/Pokemon/OtherInfo/GenderRate/GenderChart/index.tsx
@@ -0,0 +1,42 @@
+import { Chart, ChartItem, ChartConfiguration, ChartData, registerables } from 'chart.js'
+import { useEffect } from 'react'
+
+export default function GenderChart({ gender_rate }: { gender_rate: { male: number, female: number } }) {
+  const { male, female } = gender_rate
+  Chart.register(...registerables)
+
+  const labels = [
+    male > 0 ? `${male}%` : '',
+    female > 0 ? `${female}%` : ''
+  ]
+
+  const data: ChartData = {
+    labels: labels,
+    datasets: [{
+      data: [male, female]
+    }]
+  }
+
+  const config: ChartConfiguration = {
+    type: 'bar',
+    data: data,
+    options: {}
+  }
+
+  useEffect(() => {
+    const canvas = document.getElementById('gender-canvas') as HTMLCanvasElement
+    const ctx = canvas?.getContext('2d') as ChartItem
+
+    if (Chart.getChart('gender-canvas'))
+      Chart.getChart('gender-canvas')?.destroy()
+
+    const gender_chart = new Chart(ctx, config)
+  })
+
+  return (
+    <>
+      <canvas id='gender-canvas'>
+      </canvas>
+    </>
+  )
+}
--- src/pages/Pokemon/OtherInfo/GenderRate/index.tsx
+++ src/pages/Pokemon/OtherInfo/GenderRate/index.tsx
@@ -1,13 +1,23 @@
 import "./GenderRate.scss"
+import { BsGenderFemale, BsGenderMale } from 'react-icons/bs'
+import GenderChart from "./GenderChart"
 
 export default function GenderRate({ gender_rate }: { gender_rate: number }) {
   const male = (8 - gender_rate) / 0.08
   const female = gender_rate / 0.08
 
+  if (gender_rate >= 0)
+    return(
+      <>
+        {male > 0 && <BsGenderMale/>}
+        <GenderChart gender_rate = {{male, female}}/>
+        {female > 0 && <BsGenderFemale/>}
+      </>
+    )
+
   return (
     <>
-      <a>Male: {male}</a>
-      <a>Female: {female}</a>
+      Genderless
     </>
   )
 }

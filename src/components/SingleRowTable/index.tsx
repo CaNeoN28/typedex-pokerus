855cce152455773906df4d7a7d7254c6b3cd4ba1
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2022-05-31 12:30:54 -0300

    Resolvendo o border-radius e o tamanho da fonte

--- src/components/SingleRowTable/index.tsx
+++ src/components/SingleRowTable/index.tsx
@@ -9,21 +9,23 @@ interface Props {
 
 export default function SingleRowTable({ data }: Props) {
   return (
-    <table className='srt'>
-      <thead>
-        <tr>
-          {data.map((d, index) => (
-            <th key={index}>{d.label}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody className='table-data'>
-        <tr>
-          {data.map((d, index) => (
-            <td key={index}>{d.data}</td>
-          ))}
-        </tr>
-      </tbody>
-    </table>
+    <div className='srt'>
+      <table>
+        <thead>
+          <tr>
+            {data.map((d, index) => (
+              <th key={index}>{d.label}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className='table-data'>
+          <tr>
+            {data.map((d, index) => (
+              <td key={index}>{d.data}</td>
+            ))}
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }

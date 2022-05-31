4b0dbbdfb31dedb7bfa0247b971a213b8a2256c3
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2022-05-31 09:26:57 -0300

    Formatando o cabeçalho da tabela

--- src/pages/Pokemon/OtherInfo/Stats/index.tsx
+++ src/pages/Pokemon/OtherInfo/Stats/index.tsx
@@ -1,11 +1,14 @@
+import Formatting from "common/utils/string";
 import SingleRowTable from "components/SingleRowTable";
 import { PokemonStat } from "pokenode-ts";
 import StatChart from "./StatChart";
 
 export default function Stats({stats} : {stats : PokemonStat[]}){
+  const f = Formatting
+
   const data = stats.map(s => {
     return({
-      label: s.stat.name,
+      label: f.statAbbreviation(s.stat.name),
       data: s.base_stat
     })
   })

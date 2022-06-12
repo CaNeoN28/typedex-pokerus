cf1a13cd331ff0ea1251e0a57647019cc7ea3f3c
CaNeoN28 <carlosfelipe.st28@gmail.com>
2022-06-12 16:01:00 -0400

    Reaproveitando método para inserir os detalhes

--- src/pages/Pokemon/Evolution/Line/Card/EvolutionDetails/index.tsx
+++ src/pages/Pokemon/Evolution/Line/Card/EvolutionDetails/index.tsx
@@ -9,10 +9,25 @@ export default function EvolutionDetails({ evo_detail }: { evo_detail: Evolution
 
   const [details, setDetails] = useState<string[]>([])
 
+  const setDetailF = (old: string[] | undefined, item1: string, item2: string) => {
+    if (old)
+      return([...old, item1, item2])
+
+    return([item1, item2])
+  }
+
   const getDetails = () => {
-    if (evo_detail.min_level){
-      const l = evo_detail.min_level.toString()
-      setDetails(old => old ? [...old, 'Level:', l]: ['Level:', l])
+    if (ev.gender){
+      const d = ev.gender === 1 ? 'Female' : 'Male'
+      setDetails(old => setDetailF(old, 'Gender:', d))
+    }
+    if (ev.held_item){
+      const h = f.compostName(ev.held_item.name)
+      setDetails(old => setDetailF(old, 'Held Item:', h))
+    }
+    if (ev.min_level){
+      const l = ev.min_level.toString()
+      setDetails(old => setDetailF(old, 'Level:', l))
     }
     // { evo_detail.gender && <>Gender: <br />{`${evo_detail.gender === 1 ? 'Female' : 'Male'}`}</> } <br />
     // { evo_detail.held_item && <>Held Item: <br />{`${f.compostName(evo_detail.held_item.name)}`}</> } <br />
